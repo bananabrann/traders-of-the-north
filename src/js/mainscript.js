2daa6e10213c9755ab3bab10db058974c5dbc9a0
Pierson Brannan <plbrannan95@gmail.com>
2019-01-05 13:58:56 -0500

    Initial commit and main.js pseudocode work

---
+++ src/js/mainscript.js
@@ -0,0 +1,20 @@
+// An array for each resource point, that the number is altered from when the player receives the resources from the pot
+// gold: Worth one point if it is in the player's inventory
+// fish: Whoever has the most fish at the end of the turn will receive 10 points
+// skull: Loses two gold
+// rottenfish: Loses two fish
+
+let ulf = {
+    gold: 0,
+    fish: 0,
+    skull: 0,
+    rottenfish: 0
+};
+
+let sigrid = {
+    gold: 0,
+    fish: 0,
+    skull: 0,
+    rottenfish: 0
+};
+
