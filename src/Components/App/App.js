9e8e79f6308a48803fff1090955a41088f2b9856
Pierson Brannan <plbrannan95@gmail.com>
2019-03-18 18:39:33 -0400

    WORK: Added four basic play functions with pseudocode

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,13 @@ import Headerboard from "../Headerboard/Headerboard";
 import Potboard from "../Potboard/Potboard";
 import Runeboard from "../Runeboard/Runeboard";
 
+const userViking = {
+
+}
+const player2 = {
+
+}
+
 
 class App extends Component {
     constructor(props) {
@@ -18,28 +25,79 @@ class App extends Component {
             PotBadFish: 0,
 
             usersTurn: null,
+            inBet: false,
+            mustBet: false,
             manualActive: false
         };
         this.draw = this.draw.bind(this);
+        this.bet = this.bet.bind(this);
+        this.call = this.call.bind(this);
+        this.pass = this.pass.bind(this);
+
     }
 
     draw() {
         if (this.state.potTotal >= 8) {
-            console.log("I'm sorry, max cards drawn!");
+            console.log("GAME ERROR: max draw");
             // Make CSS on Draw button darkened
             return null;
-        } else{
-            this.setState({
-                potTotal: this.state.potTotal + 1
-            }, () => {
-                console.log(this.state.potTotal)
-            })
+        } else {
+            this.setState({potTotal: this.state.potTotal + 1}, () => { console.log(this.state.potTotal); });
         }
-
     }
 
+    bet() {
+        if (!this.state.inBet) {
+            this.setState({
+                inBet: true
+            })
+            console.log("'bet'");
+            // Bet logic here
+        } else {
+            console.log("GAME ERROR: 'bet' called but inBet is true");
+            // Make Bet CSS darkened and unavailable
+
+            return null;
+        }
+    }
+
+    call() {
+        if (this.state.inBet) {
+            console.log("'call'")
+            // Show the pass button CSS
+
+            // Check runes for victory
+
+            this.setState({
+                inBet: false
+            })
+
+        } else {
+            // Hide and disable the pass button
+            console.log("GAME ERROR: 'call' activated but inBet is false")
+            return null;
+        }
+    }
+
+    pass() {
+        if (this.state.inBet) {
+            console.log('passed');
+            // Show the pass button CSS
+
+            // Passes turn and forces bet
+            this.setState({
+                mustBet: true,
+                inBet: false
+            })
+        } else {
+            console.log("GAME ERROR: 'pass' called but inBet is false")
+            return null;
+        }
+    }
+
+
     componentDidUpdate() {
-        console.log("Update!")
+        // console.log("Update!");
         // Check the pot and display its pictures according to what was drawn
     }
 
@@ -47,10 +105,9 @@ class App extends Component {
         return (
             <div className="App">
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
                     <Headerboard />
                     <Potboard potTotal={this.state.potTotal} />
-                    <Buttonboard draw={this.draw} potTotal={this.state.potTotal} />
+                    <Buttonboard draw={this.draw} bet={this.bet} call={this.call} pass={this.pass} potTotal={this.state.potTotal} />
                     <Runeboard />
                 </p>
             </div>
--- src/Components/Buttonboard/Buttonboard.js
+++ src/Components/Buttonboard/Buttonboard.js
@@ -4,22 +4,25 @@ import Bet from "./Bet/Bet";
 import Scoreboard from "./Scoreboard/Scoreboard";
 
 class Buttonboard extends Component {
-   constructor(props) {
-      super(props);
-   }
+    constructor(props) {
+        super(props);
+    }
 
-   render() {
-      return (
-         <div>
+    render() {
+        return (
             <div>
-               <button onClick={this.props.draw}>Draw</button>
+                <div>
+                    <button onClick={this.props.draw}>Draw</button>
+                    <button onClick={this.props.bet}>Bet</button>
+                    <button onClick={this.props.call}>Call</button>
+                    <button onClick={this.props.pass}>Pass</button>
+                </div>
+                {/* <Draw pot={this.props.pot} /> */}
+                {/* <Bet /> */}
+                <Scoreboard />
             </div>
-            {/* <Draw pot={this.props.pot} /> */}
-            <Bet />
-            <Scoreboard />
-         </div>
-      );
-   }
+        );
+    }
 }
 
 export default Buttonboard;
