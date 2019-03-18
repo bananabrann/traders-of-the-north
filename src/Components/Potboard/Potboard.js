6cfa3b69d3bf969a7008dfc5e3991d2da8375c5e
Pierson Brannan <plbrannan95@gmail.com>
2019-03-18 19:28:17 -0400

    WORK: Added renders that display runes based on state, but leaving it now to make 'draw' draw random pieces

--- src/Components/Potboard/Potboard.js
+++ src/Components/Potboard/Potboard.js
@@ -3,18 +3,28 @@ import Badfish from "./Badfish/Badfish";
 import Badgold from "./Badgold/Badgold";
 import Fish from "./Fish/Fish";
 import Gold from "./Gold/Gold";
+import Rune from "./Rune/Rune";
+import "./Potboard.css";
 
 class Potboard extends Component {
     render() {
         let potItems = [];
-        for (let i = 0; i < this.props.potTotal; i++) {
-            potItems.push("Item ");
+
+        for (let g = 0; g < this.props.potGold; g++) {
+            return( <Gold className="pot-item gold" key={g} /> )
         }
-        console.log(potItems)
+        for (let f = 0; f < this.props.potGold; f++) {
+            return( <Fish className="pot-item fish" key={f} /> )
+        }
+
+        for (let i = 0; i < this.props.potTotal; i++) {
+            return (
+                <Rune className="pot-item" key={i}/>
+            )
+        }
 
         return (
             <div className="pot">
-                <p>{potItems}</p>
             </div>
         );
     }
---
+++ src/Components/Potboard/Rune/Rune.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Rune extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Rune;
