2daa4240a89c235207dbabf27662c8d0c0482feb
Pierson Brannan <plbrannan95@gmail.com>
2019-10-05 18:43:56 -0400

    Add handleScreenSelectionClick

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,9 +14,29 @@ class App extends Component {
       shouldDisplayGame: false,
       shouldDisplayInstructions: false
     }
+    this.handleScreenSelectionClick = this.handleScreenSelectionClick.bind(this)
   }
 
-  displayTitleScreen
+  handleScreenSelectionClick(desiredPage) {
+    // prettier-ignore
+    desiredPage === "title" ? this.setState({
+      shouldDisplayTitle: true,
+      shouldDisplayGame: false,
+      shouldDisplayInstructions: false
+    }) : null
+    // prettier-ignore
+    desiredPage === "game" ? this.setState({
+      shouldDisplayTitle: false,
+      shouldDisplayGame: true,
+      shouldDisplayInstructions: false
+    }) : null
+    // prettier-ignore
+    desiredPage === "instructions" ? this.setState({
+      shouldDisplayTitle: false,
+      shouldDisplayGame: false,
+      shouldDisplayInstructions: true
+    }) : null
+  }
 
   render() {
     return (
