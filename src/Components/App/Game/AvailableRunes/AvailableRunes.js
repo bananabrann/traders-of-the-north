b136cb154f9e261cf6b3a1ec968a7e4e94d7c302
Pierson Brannan <plbrannan95@gmail.com>
2020-02-20 15:59:07 -0500

    Available runes display in AvailableRunes correctly

--- src/Components/App/Game/AvailableRunes/AvailableRunes.js
+++ src/Components/App/Game/AvailableRunes/AvailableRunes.js
@@ -1,10 +1,18 @@
 import React from "react"
+import Rune from "../ActionBoard/Rune/Rune"
 
 class AvailableRunes extends React.Component {
   render() {
     return (
       <div>
         hello from AvailableRunes!
+        {this.props.runes.map((r, i) => {return(
+          <Rune
+            key={i}
+            runeValue={r}
+            handlePlaceRune={null}
+          />
+        )})}
       </div>
     )
   }
