a49c3438f65d0e66ecadac906c2ccb246e7e2608
Pierson Brannan <plbrannan95@gmail.com>
2020-03-19 20:06:33 -0400

    Change .prettierrc to config.js files, add default declarations

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // SECTION - Modifiables
+  tabWidth: 2,
+  singleQuote: false,
+  endOfLine: "lf",
+  semi: true,
+
+  // SECTION - Defaults
+  arrowParens: "avoid",
+  trailingComma: "none",
+};
