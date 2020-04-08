fbd26574ff69b117216af5109beb2930a35d2e6f
Pierson Brannan <plbrannan95@gmail.com>
2020-04-08 15:16:22 -0400

    Add basic About page styling

--- src/Components/App/About/About.js
+++ src/Components/App/About/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./About.scss";
+
 class About extends React.Component {
   componentDidMount() {
     document.title = "Traders of the North - About";
@@ -8,9 +10,23 @@ class About extends React.Component {
   render() {
     return (
       <div id="about">
-        <h1> About Screen</h1>
+        <h1>About</h1>
+        <div id="my-blurb">
+          <h2>Lee</h2>
+          <p>Software Developer</p>
+          {/* TODO: Add social media tags */}
+        </div>
+
+        <div id="additional-help">
+          <h3>Art - Aditya</h3>
+          <h3>Logo - Alex</h3>
+          <h3>Sound & Music - Milan Kovarbasic</h3>
+          <h3>Voice - Monica Remes</h3>
+        </div>
+
         <div onClick={() => this.props.handleScreenSelectionClick("title")}>
-          Title
+          <p>Return To Title</p>
+          <i class="fas fa-4x fa-undo"></i>
         </div>
       </div>
     );
