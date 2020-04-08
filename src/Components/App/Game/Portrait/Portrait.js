1d9b06766620be06c5e4af498e4e045a78a08dea
Pierson Brannan <plbrannan95@gmail.com>
2020-04-08 09:29:13 -0400

    Viking portraits dynamic

--- src/Components/App/Game/Portrait/Portrait.js
+++ src/Components/App/Game/Portrait/Portrait.js
@@ -10,7 +10,7 @@ class Portrait extends React.Component {
       <div>
         <img 
           className="portrait"
-          src={require("../../../../res/img/ulf/headshot.png")}   // Works
+          src={require("../../../../res/img/" + vikingName + "/headshot.png")}
           alt=""
         />
       </div>
