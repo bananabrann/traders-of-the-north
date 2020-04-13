3a9cdc03a3e4cce4e85f09d425b79f6d78181e11
Pierson Brannan <plbrannan95@gmail.com>
2020-04-13 08:43:24 -0400

    Able to run script from project root

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,13 +8,9 @@
 # You must have your AWS CLI configured. See `aws config` for setup
 # process and instructions.
 
-# NOTE
-# Yout must cd into the scripts directory.
-
-
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 npm run-script build
-aws s3 sync $DIR/../build s3://traders.bananabrann.com
+aws s3 sync $DIR/build s3://traders.bananabrann.com
 
 printf "\n\e[32m\e[1mdeploy.sh finished\033[0m\n"
