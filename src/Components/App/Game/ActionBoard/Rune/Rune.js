6b056405b86e6372bf06821dc302e38df040884a
Pierson Brannan <plbrannan95@gmail.com>
2020-04-25 16:41:40 -0400

    Code cleanup

--- src/Components/App/Game/ActionBoard/Rune/Rune.js
+++ src/Components/App/Game/ActionBoard/Rune/Rune.js
@@ -1,12 +1,4 @@
 import React from "react";
-import r2 from "../../../../../res/img/runes/2.png";
-import r3 from "../../../../../res/img/runes/3.png";
-import r4 from "../../../../../res/img/runes/4.png";
-import r5 from "../../../../../res/img/runes/5.png";
-import r6 from "../../../../../res/img/runes/6.png";
-import r7 from "../../../../../res/img/runes/7.png";
-import r8 from "../../../../../res/img/runes/8.png";
-import r9 from "../../../../../res/img/runes/9.png";
 import "./Rune.scss";
 
 class Rune extends React.Component {
@@ -16,18 +8,7 @@ class Rune extends React.Component {
   }
 
   render() {
-    const runeImgPath =
-      "../../../../../res/img/runes/" + this.props.runeValue + ".png";
-
     return (
-      // <span
-      //   className="Rune"
-      //   onClick={() => {
-      //     this.props.handlePlaceRune(this.props.runeValue);
-      //   }}
-      // >
-      //   {this.props.runeValue}
-      // </span>
       <img
         className="Rune"
         src={require(`../../../../../res/img/runes/${this.props.runeValue}.png`)}
