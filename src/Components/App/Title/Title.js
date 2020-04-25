5dc1e3c78ffca80d72fa3800d9baa696070c951e
Pierson Brannan <plbrannan95@gmail.com>
2020-04-25 11:20:01 -0400

    Button art added, logo remove

--- src/Components/App/Title/Title.js
+++ src/Components/App/Title/Title.js
@@ -1,6 +1,8 @@
 import React from "react";
-
 import "./Title.scss";
+import PLAY from "../../../res/img/buttons-menu/play/static.png";
+import INSTRUCTIONS from "../../../res/img/buttons-menu/instructions/static.png";
+import ABOUT from "../../../res/img/buttons-menu/about/static.png";
 
 const VERSION = "1.3.21";     // NOTE - Major.Minor.Patch
 
@@ -11,27 +13,29 @@ class Title extends React.Component {
         <div id="selection">
           {/* prettier-ignore */}
           <div onClick={() => this.props.handleScreenSelectionClick("game")}>
-            Play Game
+            <img src={PLAY} alt="Play Game" />
           </div>
 
           {/* prettier-ignore */}
           <div onClick={() => this.props.handleScreenSelectionClick("instructions")}>
-            Instructions
+            <img src={INSTRUCTIONS} alt="Instructions on the Game" />
           </div>
 
           {/* prettier-ignore */}
           <div onClick={() => this.props.handleScreenSelectionClick("about")}>
-            About
+            <img src={ABOUT} alt="About the Game" />
           </div>
           <p id="version">{VERSION}</p>
         </div>
 
-        <div id="logo">
+        {/* TODO - Figure out what to do with the logo */}
+        
+        {/* <div id="logo">
           <img
             src={require("../../../res/img/logo.png")}
             alt="Traders of the North game logo"
           />
-        </div>
+        </div> */}
 
         <div id="sigrid">
           <img src={require("../../../res/img/sigrid/full.png")} alt="" />
