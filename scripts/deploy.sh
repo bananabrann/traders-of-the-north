de8faa6e9922877baed59378ba8c108895d554a2
Pierson Brannan <plbrannan95@gmail.com>
2020-05-01 16:43:23 -0400

    Script execution from project root

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,12 @@
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 npm run-script build
-aws s3 sync $DIR/build s3://traders.bananabrann.com
+
+# NOTE Normally I would would the DIR, but because
+# this script is executed from project root, we need
+# to be in projcet root and not the script location
+
+# aws s3 sync $DIR/build s3://traders.bananabrann.com
+aws s3 sync build s3://traders.bananabrann.com
 
 printf "\n\e[32m\e[1mdeploy.sh finished\033[0m\n"
