bb442688475647265b7ad9cc99d9dba10ac245c3
Pierson Brannan <plbrannan95@gmail.com>
2020-12-31 16:27:23 -0500

    Consolidate Ulf and Sigrid scripts

---
+++ Assets/Sigrid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sigrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO - Make x and y coordinates relative to screen size.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Ulf.cs
+++ Assets/Ulf.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandleClick : MonoBehaviour
+public class Ulf : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // TODO - Make x and y coordinates relative to screen size.
     }
 
     // Update is called once per frame
