af2d82fab332aaa6c1860ab2aad0a99a2aa6469d
Pierson Brannan <plbrannan95@gmail.com>
2020-12-31 17:56:05 -0500

    Relocate scripts to Scripts

--- Assets/Scripts/Sigrid.cs
+++ Assets/Scripts/Sigrid.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Sigrid : MonoBehaviour
 {
+    public string charName;
+
     // Start is called before the first frame update
     void Start()
     {
         // TODO - Make x and y coordinates relative to screen size.
+
+        Debug.Log("My name is " + charName);
     }
 
     // Update is called once per frame
