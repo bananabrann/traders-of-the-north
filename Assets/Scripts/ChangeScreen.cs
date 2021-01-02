102432b0d3cc6fdfa6539c0606c802231628d084
Pierson Brannan <plbrannan95@gmail.com>
2021-01-01 22:05:45 -0500

    Add button with onClick event

---
+++ Assets/Scripts/ChangeScreen.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour
+{
+    public void Hello() {
+        Debug.Log("Hello!");
+    }
+}
