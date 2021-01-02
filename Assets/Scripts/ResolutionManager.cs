45862beab7f25d8ccf8054b24bedbce5675ce48c
Pierson Brannan <plbrannan95@gmail.com>
2021-01-01 22:26:57 -0500

    Add ResolutionManager

---
+++ Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void SetWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+
+    public void SetHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+
+    public void SetScreenResolution() {
+        // NOTE - Third parameter allows setting framerate
+        Screen.SetResolution(width, height, false);  
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ResolutionManager script has started!");
+    }
+}
