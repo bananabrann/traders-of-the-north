044bb143d593adea59329ea62528e0f9e07fb820
Pierson Brannan <plbrannan95@gmail.com>
2021-01-01 21:57:56 -0500

    Remove assets, add canvas UI

---
+++ Assets/Scripts/PlayerRuneDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRuneDisplay : MonoBehaviour
+{
+    private int health = 10;
+    public Text healthText;
+
+    void Update() {
+        healthText.text = "HEALTH: " + health;
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            health--;
+        }
+    }
+
+
+}
