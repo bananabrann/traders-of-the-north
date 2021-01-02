fc05a3b03d0ee148c7c91d6be44037e5de0b6f73
Pierson Brannan <plbrannan95@gmail.com>
2021-01-02 18:04:27 -0500

    Restructure sprites to Assets/Sprites/, and add nice menu

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public static string currentScreen;
+    
+    public void setCurrentScreen(string newScreen) {
+        // NOTE - Cannot be static.
+        currentScreen = newScreen;
+        Debug.Log("Current screen is now " + currentScreen);
+    }
+}
