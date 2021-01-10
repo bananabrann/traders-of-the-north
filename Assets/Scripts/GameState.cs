28359d99547ed8146fd97676d555870b4bfb43c4
Pierson Brannan <plbrannan95@gmail.com>
2021-01-10 17:16:19 -0500

    Add Game scene canvas and init game state

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    private static int[] playerRunes;
+    private static int[] opponentRunes;
+    private static object stockpile;
+
+    public static string whosTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whosTurn = "player";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
